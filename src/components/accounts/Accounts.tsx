039ef71da567ca269824db9c6e7e2968ec78dc29
Toushik Ahmed <pronay5271@gmail.com>
2024-07-13 21:39:58 +0600

    Add account dialog done with functionality done

--- src/components/accounts/Accounts.tsx
+++ src/components/accounts/Accounts.tsx
@@ -1,51 +1,69 @@
-'use client';
+"use client";
 // Accounts.tsx
 
-import { useState } from 'react';
-import { IoIosRefresh } from 'react-icons/io';
-import { IoAdd } from 'react-icons/io5';
-import { CardWithForm } from '../addCard/CardWithForm';
-import { Button } from '../ui/button';
+import { useState } from "react";
+import { IoIosRefresh } from "react-icons/io";
+import { IoAdd } from "react-icons/io5";
+import { CardInfo, CardWithForm } from "../addCard/CardWithForm";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 type Props = {};
 
 function Accounts({}: Props) {
-  const [showForm, setShowForm] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-  const toggleForm = () => {
-    setShowForm(!showForm);
-  };
-
-  const handleCloseForm = () => {
-    setShowForm(false); // Close the form
+  const handleCloseForm = (value: CardInfo) => {
+    // Backend Call with card info
+    console.log("In accounts", value);
+    setDialogOpen(false);
   };
 
   return (
     <div>
-      <div className="flex justify-end gap-2">
-        {!showForm && (
+      <Dialog
+        open={dialogOpen}
+        onOpenChange={(ev) => {
+          setDialogOpen(ev);
+          console.log(ev);
+        }}
+      >
+        <div className="flex justify-end gap-2">
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              className="flex text-white hover:text-white gap-2 bg-green-500 hover:bg-green-600"
+            >
+              <IoAdd className="h-4 w-4" /> Add Account
+            </Button>
+          </DialogTrigger>
+
           <Button
-            onClick={toggleForm}
             variant="outline"
-            className="flex text-white hover:text-white gap-2 bg-green-500 hover:bg-green-600"
+            size="icon"
+            className="text-green-500 border-green-500 hover:text-green-500"
           >
-            <IoAdd className="h-4 w-4" /> Add Account
+            <IoIosRefresh className="h-4 w-4" />
           </Button>
-        )}
-        <Button
-          variant="outline"
-          size="icon"
-          className="text-green-500 border-green-500 hover:text-green-500"
-        >
-          <IoIosRefresh className="h-4 w-4" />
-        </Button>
-      </div>
-      <div className="mt-5 flex flex-col items-center">
-        <div className="text-center mb-4">
-          All your accounts information here
         </div>
-        <div>{showForm && <CardWithForm onClose={handleCloseForm} />}</div>
-      </div>
+        <div className="mt-5 flex flex-col items-center">
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-2">Add Account</DialogTitle>
+              <DialogDescription asChild>
+                <CardWithForm onSubmit={handleCloseForm} />
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </div>
+      </Dialog>
     </div>
   );
 }
--- src/components/addCard/CardWithForm.tsx
+++ src/components/addCard/CardWithForm.tsx
@@ -1,61 +1,101 @@
-'use client';
+"use client";
 // CardWithForm.tsx
 
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select';
+} from "@/components/ui/select";
+import { useState } from "react";
+import { DialogClose } from "../ui/dialog";
+import valid from "card-validator";
+
+export interface CardInfo {
+  cardNumber: string;
+  bankName: string;
+  accountType: "credit" | "debit";
+  transfarredAmount: number;
+}
 
 type Props = {
-  onClose: () => void; // Callback to close the form
+  onSubmit: (val: CardInfo) => void; // Callback to close the form
 };
 
-export function CardWithForm({ onClose }: Props) {
-  const handleCancel = () => {
-    onClose(); // Call onClose callback to close the form
+export function CardWithForm({ onSubmit }: Props) {
+  const [cardInfo, setCardInfo] = useState<CardInfo>({
+    cardNumber: "",
+    bankName: "",
+    accountType: "credit",
+    transfarredAmount: 0,
+  });
+
+  const [invalidCardnumber, setInvalidCardNumber] = useState(false);
+
+  const onValueChange = (key: keyof CardInfo, value: any) => {
+    console.log(key, value);
+    setCardInfo({
+      ...cardInfo,
+      [key]: value,
+    });
   };
 
   const handleAdd = () => {
-    // Handle form submission here if needed
-    onClose(); // Close the form after submission
+    const isCreditValid = valid.number(cardInfo.cardNumber);
+    setInvalidCardNumber(!isCreditValid.isValid);
+    if (!isCreditValid.isValid) return;
+
+    onSubmit(cardInfo);
   };
 
   return (
-    <Card className="w-[350px] ">
-      <CardHeader className="text-center">
+    <Card className="w-full ">
+      {/* <CardHeader className="text-center">
         <CardTitle>Transfer Your Money</CardTitle>
         <CardDescription>
           Add money from any existing card or bank.
         </CardDescription>
-      </CardHeader>
-      <CardContent>
+      </CardHeader> */}
+      <CardContent className="mt-4">
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="accountName">Account Name</Label>
-              <Input id="accountName" placeholder="Credit card-xxx" />
+              <Input
+                value={cardInfo.cardNumber}
+                onChange={(ev) => {
+                  onValueChange("cardNumber", ev.target.value);
+                  setInvalidCardNumber(false);
+                }}
+                id="cardNumber"
+                placeholder="Credit card-xxx"
+              />
+              {invalidCardnumber && (
+                <div className="ml-2 left-align text-xs text-red-500">
+                  Invalid Card Number
+                </div>
+              )}
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="bankName">Bank Name</Label>
-              <Input id="bankName" placeholder="Bank name " />
+              <Input
+                value={cardInfo.bankName}
+                onChange={(ev) => onValueChange("bankName", ev.target.value)}
+                id="bankName"
+                placeholder="Bank name "
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="accountType">Account Type</Label>
-              <Select>
+              <Select
+                value={cardInfo.accountType}
+                onValueChange={(val) => onValueChange("accountType", val)}
+              >
                 <SelectTrigger id="accountType">
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
@@ -67,15 +107,24 @@ export function CardWithForm({ onClose }: Props) {
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="transferedMoney">Transferred Money</Label>
-              <Input id="transferedMoney" placeholder="0$" />
+              <Input
+                onFocus={(ev) => ev.target.select()}
+                value={cardInfo.transfarredAmount}
+                onChange={(ev) =>
+                  onValueChange("transfarredAmount", ev.target.value)
+                }
+                id="transferedMoney"
+                placeholder="0$"
+                type="number"
+              />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="outline" onClick={handleCancel}>
-          Cancel
-        </Button>
+        <DialogClose asChild>
+          <Button variant="outline">Cancel</Button>
+        </DialogClose>
         <Button onClick={handleAdd}>Add</Button>
       </CardFooter>
     </Card>
