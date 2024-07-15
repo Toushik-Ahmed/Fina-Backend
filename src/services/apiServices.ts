87c818445b27a6d978e6a602b1cd2c04da8a7a31
Toushik-Ahmed <pronay5271@gmail.com>
2024-07-15 15:13:07 +0600

    add account card posts

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -1,9 +1,14 @@
 import { CardInfo } from '@/components/addCard/CardWithForm';
 import axios from 'axios';
+import { getToken } from './TokenService';
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URI;
 
+export const addAccount = async (data: CardInfo) => {
+  const add = await axios.post(`${baseUrl}/accounts/create`, data,{
 
-export const addAccount = async (data:CardInfo ) => {
-  const add = await axios.post(`${baseUrl}/accounts/create`, data);
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
   return add.data;
 };
