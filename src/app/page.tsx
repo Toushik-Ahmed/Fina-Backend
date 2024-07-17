e9ed55599d254bfbb78b5f03f22d8da1ba2e1ea9
Toushik-Ahmed <Pronay5271@gmail.com>
2024-07-17 22:19:02 +0600

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,269 @@
-import Image from "next/image";
+import HomePage from '@/components/homepage/HomePage';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { IoIosRocket } from 'react-icons/io';
+import Logo from '../assets/logo.png';
 
-export default function Home() {
+type Props = {};
+
+const page = (props: Props) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-[#0f1111] h-fit p-4">
+      <div className="flex justify-between items-start fixed top-0 left-0 right-0 p-4 bg-transparent backdrop-blur-[2px]">
+        <div className="flex items-center gap-2 ">
+          <Image
+            width={64}
+            height={64}
+            src={Logo}
+            alt="Fina Logo"
+            className="rounded"
+          />
+          <div className="text-4xl font-sans font-bold text-white">Fina</div>
+        </div>
+        <div className=" flex justify-center pl-4 ">
+          <HomePage />
+        </div>
+        <div className="flex gap-4">
+          <Link href="/login">
+            <Button
+              className="bg-inherit text-white hover:text-[#3ef194]"
+              variant="outline"
+            >
+              Log In
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button
+              type="submit"
+              className="bg-[#3ef194] gap-2"
+              variant="outline"
+            >
+              <IoIosRocket className="text-black" />
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-[10vh]">
+        <div className="w-fit p-1 text-white border-2 border-[#3ef194] rounded-2xl">
+          For those who care about their future
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-[10vh]">
+        <div className="w-fit p-1 text-white text-6xl text-center font-extrabold">
+          <p>Build Your</p>
+          <p>Own Tracking</p>
+          <p>System</p>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center mt-[4vh]">
+        <div className="flex-col text-center">
+          <Link href="/signup">
+            <Button
+              className="bg-[#3ef194] gap-2 p-[2vw] text-2xl"
+              variant="outline"
+            >
+              <IoIosRocket className="text-black" />
+              Get Started For Free
+            </Button>
+          </Link>
+          <p className="text-white mt-4">no credit card required</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-center items-center mt-[10vh] mb-[5vh]">
+        <div className="w-[70vw] p-1  rounded-2xl flex-col text-center space-y-4">
+          <div className="text-[#3ef194] font-medium text-xl">How It Works</div>
+          <div className="text-white text-6xl">Enjoy Peace Of Mind</div>
+          <div className="text-white text-xl">
+            <div>Build your own custom tracking system once,</div>
+            <div>enjoy financial clarity forever.</div>
+          </div>
+          <div className="text-white flex justify-between  gap-10">
+            <div>
+              <div className="text-8xl">1</div>
+              <div className="text-2xl">Add</div>
+              <div>
+                Connect live data from 12,000+ financial institutions &
+                integrations or import your data manually.
+              </div>
+            </div>
+            <div>
+              <div className="text-8xl">2</div>
+              <div className="text-2xl">Build</div>
+              <div>
+                Set categories, rules, & clean transactions with ease. Build
+                custom tracking scenarios with unique formulas.
+              </div>
+            </div>
+            <div>
+              <div className="text-8xl">3</div>
+              <div className="text-2xl">share</div>
+              <div>
+                Publish your financial tracking system and help others manage
+                their money how you do. Fina is like lego blocks for your
+                finances.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+      <div
+        id="features"
+        className="flex justify-center items-center mt-[10vh] mb-[5vh]"
+      >
+        <div className="w-[70vw] p-1  rounded-2xl flex-col text-center space-y-4">
+          <div className="text-[#3ef194] font-medium text-xl">Features</div>
+          <div className="text-white text-6xl">
+            <div>The Pursuit Of </div>
+            <div>Financial Freedom</div>{' '}
+          </div>
+          <div className="text-white text-xl ">
+            <div>No matter where you are on your journey, Fina can help.</div>
+          </div>
+          <div className="text-white flex justify-between  gap-10">
+            <div className="bg-white text-black p-4 max-w-[25vw] h-fit">
+              <div className="text-2xl font-bold ">
+                Unlimited Custom Categories
+              </div>
+
+              <div className="mt-4">
+                Create as many categories as you need! Don't worry, we have
+                emojis to give them some flare
+              </div>
+            </div>
+
+            <div className="bg-white text-black p-4 max-w-fit ">
+              <div className="text-2xl font-bold">
+                Flexible Budgeting & Scenario Planning
+              </div>
+
+              <div className="mt-4">
+                Create budgets or forecasts in ways that match your lifestyle.
+                Fina supports all kinds of unique budgeting scenarios from
+                simple budgets, to zero-based, rollovers, and more. You can even
+                set budgets and goals for custom metrics beyond categories. Get
+                any level of insight you need based on your current stage of
+                life.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-[10vh] mb-[5vh]">
+        <div className="w-[70vw] p-1  rounded-2xl flex-col text-center space-y-4">
+          <div className="text-[#3ef194] font-medium text-xl">Why Fina?</div>
+          <div className="text-white text-6xl">
+            <div>The Pursuit Of </div>
+            <div>Financial Freedom</div>{' '}
+          </div>
+          <div className="text-white text-xl ">
+            <div>No matter where you are on your journey, Fina can help.</div>
+          </div>
+          <div className="text-white flex justify-between  gap-10">
+            <div className="bg-white text-black p-4">
+              <div className="text-2xl font-bold ">
+                Unlimited Custom Categories
+              </div>
+
+              <div className="mt-4">
+                Create as many categories as you need! Don't worry, we have
+                emojis to give them some flare
+              </div>
+            </div>
+
+            <div className="bg-white text-black p-4">
+              <div className="text-2xl font-bold">
+                Flexible Budgeting & Scenario Planning
+              </div>
+
+              <div className="mt-4">
+                Create budgets or forecasts in ways that match your lifestyle.
+                Fina supports all kinds of unique budgeting scenarios from
+                simple budgets, to zero-based, rollovers, and more. You can even
+                set budgets and goals for custom metrics beyond categories. Get
+                any level of insight you need based on your current stage of
+                life.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        id="testimonial"
+        className="flex justify-center items-center mt-[10vh] mb-[5vh]"
+      >
+        <div className="w-[70vw] p-1  rounded-2xl flex-col text-center space-y-4">
+          <div className="text-[#3ef194] font-medium text-xl">Testimonial</div>
+          <div className="text-white text-6xl">
+            <div>What The People Say</div>{' '}
+          </div>
+          <div className="text-white text-xl">
+            <div>Join a community of like-minded individuals.</div>
+          </div>
+          <div className="text-white flex justify-around  gap-10">
+            <div className="bg-black text-black bg-[#0f1111] border-4 max-w-[20vw]">
+              {/* <div className="p-4"> */}
+              <div className=" text-white p-4">
+                Tracking my money helped me go from $260,000 debt to millionaire
+                in 10 years. Fina is my new favorite financial tool.
+              </div>
+              <div className="bg-white mt-4 font-bold">
+                {' '}
+                <div>-Cori Arnold </div>
+                <div>@iamcoriarnold</div>
+                {/* </div> */}
+              </div>
+            </div>
+
+            <div className="bg-black text-black bg-[#0f1111] border-4 max-w-[20vw]">
+              {/* <div className="p-4"> */}
+              <div className=" text-white p-4">
+                It’s not about what you make, it’s about what you keep. I've
+                been loving Fina so far.
+              </div>
+              <div className="bg-white mt-4 font-bold">
+                {' '}
+                <div>-Dividend Hero </div>
+                <div>@HeroDividend</div>
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+          <div className="text-white flex justify-around  gap-10">
+            <div className="bg-black text-black bg-[#0f1111] border-4 max-w-[20vw]">
+              {/* <div className="p-4"> */}
+              <div className=" text-white p-4">
+                I went from never tracking my finances to checking Fina all the
+                time. I feel so much more in control of my money now.
+              </div>
+              <div className="bg-white mt-4 font-bold">
+                {' '}
+                <div>-Matt King </div>
+                <div>@mattking20</div>
+                {/* </div> */}
+              </div>
+            </div>
+
+            <div className="bg-black text-black bg-[#0f1111] border-4 max-w-[20vw]">
+              {/* <div className="p-4"> */}
+              <div className=" text-white p-4">
+                I just started using Fina and it's amazing! I've been wanting to
+                create a budget and this is the perfect tool for it.
+              </div>
+              <div className="bg-white mt-4 font-bold">
+                {' '}
+                <div>-Tommy Guta </div>
+                <div>@tommyguta</div>
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default page;
--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -1,7 +1,104 @@
-type Props = {};
+import {
+  Menubar,
+  MenubarContent,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
 
-function HomePage({}: Props) {
-  return <div>homepage</div>;
+export default function Homepage() {
+  return (
+    <Menubar className="w-fit bg-black text-white font-medium gap-4  ">
+      <MenubarMenu>
+        <MenubarTrigger className="hover:text-[#3ef194]">
+          <a href="#features">Features </a>
+        </MenubarTrigger>
+        {/* <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            New Window <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled>New Incognito Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Share</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Email link</MenubarItem>
+              <MenubarItem>Messages</MenubarItem>
+              <MenubarItem>Notes</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>
+            Print... <MenubarShortcut>⌘P</MenubarShortcut>
+          </MenubarItem>
+        </MenubarContent> */}
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger className="hover:text-[#3ef194]">
+          <a href="#testimonial"> Testimonial</a>
+        </MenubarTrigger>
+        {/* <MenubarContent>
+          <MenubarItem>
+            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Find</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Search the web</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Find...</MenubarItem>
+              <MenubarItem>Find Next</MenubarItem>
+              <MenubarItem>Find Previous</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>Cut</MenubarItem>
+          <MenubarItem>Copy</MenubarItem>
+          <MenubarItem>Paste</MenubarItem>
+        </MenubarContent> */}
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger className="hover:text-[#3ef194]">
+          Pricing
+        </MenubarTrigger>
+        {/* <MenubarContent>
+          <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+          <MenubarCheckboxItem checked>
+            Always Show Full URLs
+          </MenubarCheckboxItem>
+          <MenubarSeparator />
+          <MenubarItem inset>
+            Reload <MenubarShortcut>⌘R</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled inset>
+            Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem inset>Hide Sidebar</MenubarItem>
+        </MenubarContent> */}
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger className="hover:text-[#3ef194]">
+          Resources
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarRadioGroup value="benoit">
+            <MenubarRadioItem value="andy">Vision</MenubarRadioItem>
+            <MenubarRadioItem value="benoit">Demo</MenubarRadioItem>
+            <MenubarRadioItem value="Luis">Blog</MenubarRadioItem>
+          </MenubarRadioGroup>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
 }
-
-export default HomePage;
